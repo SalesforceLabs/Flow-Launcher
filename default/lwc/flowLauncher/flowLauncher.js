dc0f415aac60a32140ca8ee08f6da2909cd6d9c5
Josh Dayment <52637666+jdayment@users.noreply.github.com>
2025-01-28 08:52:49 -0700

    Update flowLauncher.js

--- default/lwc/flowLauncher/flowLauncher.js
+++ default/lwc/flowLauncher/flowLauncher.js
@@ -18,16 +18,26 @@
 *   01/06/25 - Eric Smith  
 *   Added modalSize attribute (small, medium, large(default), full)
 * 
+*  01/28/25 - Josh Dayment
+*  Added CPE
 */
 
 import { LightningElement, api, track } from 'lwc';
 import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 import flowModal from 'c/flowModal';
 
+
+
 export default class FlowLauncher extends LightningElement {
 
+    
+
     @api buttonLabel;
-    @api showFlowInModal;
+    @api
+    get showFlowInModal() {
+       return this.cb_showFlowInModal === 'CB_TRUE';
+    }
+    @api cb_showFlowInModal;
     @api flowToLaunch;
     @api flowParams = [];
     @api flowFinishBehavior;
@@ -37,10 +47,22 @@ export default class FlowLauncher extends LightningElement {
     @api iconName;
     @api buttonVariant;
     @api iconPosition;
-    @api stretchButton;
+    @api 
+    get stretchButton() {
+        return this.cb_stretchButton === 'CB_TRUE';
+    }
+    @api cb_stretchButton;
     @api buttonPadding = 'slds-p-around_small';
-    @api isDisableClose = false;
-    @api hideButton = false;
+    @api 
+    get isDisableClose() {
+        return this.cb_isDisableClose === 'CB_TRUE';
+    };
+    @api cb_isDisableClose;
+    @api 
+    get hideButton() {
+        return this.cb_hideButton === 'CB_TRUE';
+    };
+    @api cb_hideButton;
     @api modalSize = 'large';
     // @api INPUT_Record;
     // @api INPUT_Collection;
@@ -98,7 +120,7 @@ export default class FlowLauncher extends LightningElement {
     // @api OUTPUT_Record = {};
     // @api OUTPUT_Collection = [];
 
-    handleButtonClick() {
+    handleButtonClick() {        
         if (this.showFlowInModal) {
             this.handleOpenModal();
         } else {
